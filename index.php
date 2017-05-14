d9bea1d93b66aedae5c7dfd0b45610c584176bba
projetouniversidadegame2d <guiholiv0@gmail.com>
2017-05-14 19:21:36 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <title>Jogo web - projeto de introdução a engenharia</title>
+    <meta name="description" content="Jogo web criado em javascript e html baseado no bomberman">
+    <meta name="Keywords" content="jogo web,projeto faculdade,bomberman">
+    <meta name="author" content="World Tech">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="libraries/stylesheet.css">
+</head>
+
+
+<body>
+	<header>
+
+
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Sobre nós</li>
+                <li>Fale conosco</li>
+                <li>Game</li>
+            </ul>
+        </nav>
+
+    </header>
+
+    <main>
+<p>aqui fica o corpo do site</p>
+
+    </main>
+
+    <footer>
+        <p>aqui fica o rodapé</p>
+    </footer>
+</body>
+</html>
